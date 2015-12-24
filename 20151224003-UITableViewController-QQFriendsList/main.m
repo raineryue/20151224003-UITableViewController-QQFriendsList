5748d3c56c4a52c0926d89fce871126519b3430b
raineryue <jinlei_yue@sina.com>
2015-12-24 16:46:37 +0800

    Initial Commit

---
+++ 20151224003-UITableViewController-QQFriendsList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  20151224003-UITableViewController-QQFriendsList
+//
+//  Created by Rainer on 15/12/24.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
